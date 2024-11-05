ce7b9a159b6f0e6fce36d1aa8d14a1ee8ba05812
MashalloA <mashalloalim07@gmail.com>
2024-11-05 20:07:55 +0600

    hw

---
+++ handlers/start.py
@@ -0,0 +1,32 @@
+from aiogram import Router, types
+from aiogram.filters import Command
+from aiogram.types import InlineKeyboardMarkup
+
+start_router = Router()
+id_list = []
+
+@start_router.message(Command('start'))
+async def start_handler(message: types.Message):
+    name = message.from_user.first_name
+    msg = f"Привет {name}"
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+        [
+            types.InlineKeyboardButton(
+                text="my profile inst",
+                url="https://instagram.com/lllsuccesslll",
+            )
+        ],
+        [
+            types.InlineKeyboardButton(
+                text="my tg channel",
+                url="t.me/+1pVI9RuurtQzY2Qy",
+            )
+        ]
+    ]
+)
+    await message.answer(msg, reply_markup=kb)
+    us_id = message.from_user.id
+    if us_id not in id_list:
+        id_list.append(us_id)
+    await message.answer(f"my bot was used by {len(id_list)} users")
--- main.py
+++ main.py
@@ -1,37 +1,15 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from dotenv import dotenv_values
-from random import choice
 
-token = dotenv_values(".env")["BOT_TOKEN"]
-bot = Bot(token=token)
-dp = Dispatcher()
-id_list = []
+from bot_config import bot, dp
+from handlers.echo import echo_router
+from handlers.start import start_router
+from handlers.info import info_router
 
-@dp.message(Command('start'))
-async def start_handler(message: types.Message):
-    name = message.from_user.first_name
-    await message.answer(f"Привет {name}")
-    us_id = message.from_user.id
-    if us_id not in id_list:
-        id_list.append(us_id)
-    await message.answer(f"my bot was used by {len(id_list)} users")
-
-@dp.message(Command('myinfo'))
-async def start_handler(message: types.Message):
-    name = message.from_user.first_name
-    us_id = message.from_user.id
-    us_name = message.from_user.username
-    await message.answer(f"your id is: {us_id}, your name is: {name}, your username is: {us_name}")
-
-@dp.message(Command('random'))
-async def start_handler(message: types.Message):
-    names = ["alex", "edward", "max", "daniel"]
-    name = choice(names)
-    await message.answer(f"random name in list: {name}")
 
 async def main():
+    dp.include_router(start_router)
+    dp.include_router(info_router)
+    dp.include_router(echo_router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
