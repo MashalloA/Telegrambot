34cd725f94be283dd7f9052ca97c7e89f81c70e8
MashalloA <mashalloalim07@gmail.com>
2024-11-07 14:18:20 +0600

    .

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from handlers.info import info_router
 async def main():
     dp.include_router(start_router)
     dp.include_router(info_router)
-    dp.include_router(echo_router)
     dp.include_router(rnd_router)
+    dp.include_router(echo_router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
