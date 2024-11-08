224462e767e1bb3fe4d383515be278deffad1fa3
MashalloA <mashalloalim07@gmail.com>
2024-11-08 23:59:01 +0600

    hw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 
-from handlers.random import rnd_router
+from dialog import dialogue_router
+from handlers.random import random_router
 from bot_config import bot, dp
 from handlers.echo import echo_router
 from handlers.start import start_router
@@ -10,7 +11,8 @@ from handlers.info import info_router
 async def main():
     dp.include_router(start_router)
     dp.include_router(info_router)
-    dp.include_router(rnd_router)
+    dp.include_router(random_router)
+    dp.include_router(dialogue_router)
     dp.include_router(echo_router)
     await dp.start_polling(bot)
 
